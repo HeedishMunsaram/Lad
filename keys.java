b377f5e965c2525630553c7a4e33a9e20937b0c3
Heedish Munsaram <54936388+HeedishMunsaram@users.noreply.github.com>
2019-09-27 14:37:08 +0400

    Add files via upload

---
+++ keys.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class keys {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Scanner keyboard = new Scanner(System.in); 
+		
+	System.out.println("Enter the number of pods followed by");
+    System.out.println("the number of peas in a pod:");
+    
+    int numberOfPods = keyboard.nextInt( );
+    int peasPerPod = keyboard.nextInt( );
+
+    int totalNumberOfPeas = numberOfPods*peasPerPod;
+    
+    System.out.print(numberOfPods + " pods and ");
+    System.out.println(peasPerPod + " peas per pod.");
+    System.out.println("The total number of peas = " + totalNumberOfPeas);
+    
+    keyboard.close();
+    
+		
+	}
+
+}
